12b4d994d40797f7d5495f9311f8a86e9385632b
Cunhaisis <isis.cunha13@gmail.com>
2020-11-13 21:01:34 -0300

    Demo - recuperar dados

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -1,15 +1,40 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection(); // conecta no banco de dados
+			
+			st = conn.createStatement(); 
+			rs = st.executeQuery("Select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+		finally {
+			rs.close();
+			st.close();
+			DB.closeConnection();
+		}
+		
 
 	}
 
